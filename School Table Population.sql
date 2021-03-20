e81fdd4840641c0583c1dc13a27460d849009455
filipecamargos <filipe.atrabalho@gmail.com>
2021-03-20 12:49:32 -0600

    contact add

--- School Table Population.sql
+++ School Table Population.sql
@@ -18,13 +18,29 @@ VALUES ('21 116th Street E', 'Lewis', 'Washington', 'US','98377'),
 		('47 Broken Street E ', 'Lewis', 'Washington', 'US','98377'),
 		('101 Broken 2nd Street E ', 'Lewis', 'Washington', 'US','98377');
 
+/*Test*/
+SELECT * FROM address;
+
 /*******************************************************
-*
+*  Add date to the Contact Table
 ********************************************************/
-
+INSERT INTO contact (address_id, phone,   email, last_updated)
+VALUES ( 1, '123-456-789', 'a@a.com', now()),
+		(2, '223-456-789', 'b@a.com', now()),
+		(3, '323-456-789', 'c@a.com', now()),
+		(4, '423-456-789', 'd@a.com', now()),
+		(5, '523-456-789', 'e@a.com', now()),
+		(6, '623-456-789', 'f@a.com', now()),
+		(7, '723-456-789', 'g@a.com', now()),
+		(8,  '823-456-789', 'h@a.com', now()),
+		(9, '923-456-789', 'i@a.com', now()),
+		(10, '103-456-789', 'j@a.com', now());
+        
+/*Test*/
+SELECT * FROM contact;
 
 /*******************************************************
-*
+* Add date to the Departament Table
 ********************************************************/
 
 
